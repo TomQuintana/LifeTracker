a40c8b50a7f3cdf0e51074ee9f7e7cb7a566b18a
tomQuintana <tomas.quintana@gojiraf.com>
2024-04-23 00:03:53 -0300

    feat: obtain a cotization calling a cotization service

--- src/services/expense_service.py
+++ src/services/expense_service.py
@@ -2,6 +2,7 @@ from sqlmodel.ext.asyncio.session import AsyncSession
 
 from ..domain.expenses.model import Expense
 from ..domain.expenses.requestModel import ExpenseRequest
+from .cotization import Cotization
 
 
 class ExpenseService:
@@ -9,7 +10,13 @@ class ExpenseService:
         self.session = session
 
     async def create_expense(self, expense_data: ExpenseRequest):
+
+        crypto_currency = 'USDT'
+        cotization_service = Cotization(crypto_currency)
+        cotization = await cotization_service.calculate_cotization(expense_data.price_ARS)
+
         new_expense = Expense(**expense_data.model_dump())
+        new_expense.price_USDT = cotization
 
         self.session.add(new_expense)
 
