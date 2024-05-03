90fbfce5633dbaa4d20288a4944af1f5607ea069
tomQuintana <tomas.quintana@gojiraf.com>
2024-05-03 00:44:10 -0300

    ✨ Add month and change query
    modify a query for obtain all data of month pass in request header

--- src/services/expense_service.py
+++ src/services/expense_service.py
@@ -10,21 +10,21 @@ class ExpenseService:
     def __init__(self, session: AsyncSession):
         self.session = session
 
+    # NOTE: define a return type for principal methods
+    async def _create_expense_db(self, expense_data):
+        self.session.add(expense_data)
+        await self.session.commit()
+
     async def create_expense(self, expense_data: ExpenseRequest):
         crypto_currency = "USDT"
         cotization_service = Cotization(crypto_currency)
         cotization = await cotization_service.calculate_cotization(expense_data.price_ARS)
 
-        new_expense = Expense(**expense_data.model_dump())
-        new_expense.price_USDT = cotization
+        expense = Expense(**expense_data.model_dump())
+        expense.price_USDT = cotization
 
-        self.session.add(new_expense)
+        return await self._create_expense_db(expense)
 
-        await self.session.commit()
-
-        return new_expense
-
-    # TODO: define type of data
     async def _obtatin_total(self, data) -> dict[str, float]:
         total_spend_ars = 0
         total_spend_usdt = 0
@@ -40,9 +40,10 @@ class ExpenseService:
 
         return data_reponse
 
-    async def calculate_total(self):
-        statament = select(Expense)
-        data = await self.session.exec(statament)
+    async def calculate_total(self, month: int):
+        month_expense = int(month)  # REF: pass a query separeted method
+        query = select(Expense).where(Expense.month == month_expense)
+        data = await self.session.exec(query)
         return await self._obtatin_total(data)
 
     def process_data(self, data):
@@ -59,7 +60,8 @@ class ExpenseService:
             data_reponse.append(data_list)
         return data_reponse
 
-    async def obtain_data(self):
-        statament = select(Expense)  # NOTE: improve statament for dont get uuid
-        data = await self.session.exec(statament)
+    async def obtain_data(self, month: int):
+        month_expense = int(month)  # REF: pass a separeted method
+        query = select(Expense).where(Expense.month == month_expense)
+        data = await self.session.exec(query)
         return self.process_data(data)
