bb9f771d9f19c409823a9b2a1e312cfeb5a47c3f
tomQuintana <tomas.quintana@gojiraf.com>
2024-05-03 00:41:00 -0300

    🎨 Add month row and calculate month and date using datetime library

--- src/domain/expenses/model.py
+++ src/domain/expenses/model.py
@@ -1,15 +1,20 @@
 from uuid import UUID, uuid4
+from datetime import datetime
 
 import sqlalchemy.dialects.postgresql as pg
 from sqlmodel import Column, Field, SQLModel
 
+month = datetime.now().month
+date_expense = datetime.now().date()
+
 
 class Expense(SQLModel, table=True):
-    __tablename__ = "expenses"
+    __tablename__ = "expenses"  # NOTE: unnalaible problem
     uuid: UUID = Field(sa_column=Column(pg.UUID, primary_key=True, unique=True, default=uuid4))
     name: str
     price_ARS: float
     price_USDT: float | None = None
     type: str | None = None
     coutes: int | None = None
-    date: str
+    month: int = datetime.now().month
+    date: datetime = datetime.now().date()
