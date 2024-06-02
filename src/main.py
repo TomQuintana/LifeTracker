0f262cdfca5d41c75141fc0bd54b2d79ec0ca9c6
tomQuintana <tomas.quintana@gojiraf.com>
2024-06-02 14:33:38 -0300

    🎨 Add routers of books and users

--- src/main.py
+++ src/main.py
@@ -4,6 +4,8 @@ from fastapi import FastAPI
 
 from .db.main import init_db
 from .routes.expense import router as expense_router
+from .routes.user import router as user_router
+from .routes.book import router as book_router
 
 
 @asynccontextmanager
@@ -16,3 +18,5 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(title="Life Tracker", lifespan=lifespan)
 app.include_router(expense_router)
+app.include_router(user_router)
+app.include_router(book_router)
