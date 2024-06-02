ca4aebbdb515192ac18fe554ba06d27e25894de2
tomQuintana <tomas.quintana@gojiraf.com>
2024-06-02 14:34:08 -0300

    🎨 Add two new enviroments

--- src/config/config.py
+++ src/config/config.py
@@ -3,6 +3,8 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     POSTGRESQL_URL: str
+    SECRECT_KEY: str
+    ALGORITHM: str
 
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
 
