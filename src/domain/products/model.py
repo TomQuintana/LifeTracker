03740eaa1b7c8004f3f4ecf92a5fa6c8991334ab
tomQuintana <tomas.quintana@gojiraf.com>
2024-07-29 17:48:09 -0300

    ✨ Add schema for products

---
+++ src/domain/products/model.py
@@ -0,0 +1,13 @@
+from uuid import UUID, uuid4
+
+import sqlalchemy.dialects.postgresql as pg
+from sqlmodel import Column, Field, SQLModel
+
+
+class Products(SQLModel, table=True):
+    __tablename__ = "products"
+    uuid: UUID = Field(sa_column=Column(pg.UUID, primary_key=True, unique=True, default=uuid4))
+    name: str
+    price_ARS: float
+    price_USDT: float | None = None
+    expense_id: UUID = Field(default=None, foreign_key="expenses.uuid")
