15da96cebfe0bab911f61bf69391091ec8f3e978
tomQuintana <tomas.quintana@gojiraf.com>
2024-08-10 17:34:00 -0300

    🎨 Implement queries functions

--- src/services/book.py
+++ src/services/book.py
@@ -2,43 +2,57 @@ from sqlmodel import select
 from sqlmodel.ext.asyncio.session import AsyncSession
 
 from src.domain.book.model import Book
-from src.utils.format_data import format_data_list, format_data_dict
 from src.utils.alert import not_found_resource
+from src.utils.format_data import format_data_dict, format_data_list
+
+from ..repository.queries import (
+    create_data_db,
+    delete_books_by_uuid,
+    get_books_by_filter,
+    get_books_db,
+)
 
 
 class BookService:
     def __init__(self, session: AsyncSession):
         self.session = session
 
-    # REF: pass this to general service
-    async def _create_data_db(self, data: Book) -> None:
-        self.session.add(data)
-        return await self.session.commit()
-
     async def create_book(self, book_model) -> Book:
         book = Book(**book_model.model_dump())
-        await self._create_data_db(book)
+
+        try:
+            await create_data_db(self.session, book)
+        except Exception as e:
+            raise e
+
         return book
 
     async def get_books(self):
-        query = select(Book)
-        book_data = await self.session.exec(query)
+        try:
+            book_data = await get_books_db(self.session)
+        except Exception as e:
+            raise e
+
         book_response = format_data_list(book_data)
         return book_response
 
     async def get_books_by_filter(self, uuid: str):
-        query = select(Book).where(Book.uuid == uuid)
-        book_data = await self.session.exec(query)
-        book_response = format_data_dict(book_data)
-        return book_response
+        try:
+            book_data = await get_books_by_filter(self.session, uuid)
+            book_response = format_data_dict(book_data)
+            return book_response
+
+        except Exception as e:
+            raise e
 
     async def delete_books_by_uuid(self, uuid: str):
-        query = select(Book).where(Book.uuid == uuid)
-        result = await self.session.exec(query)
-        book_to_delete = result.one()
-        await self.session.delete(book_to_delete)
-        await self.session.commit()
-        return book_to_delete
+        try:
+            await get_books_by_filter(self.session, uuid)
+            book_to_delete = await delete_books_by_uuid(self.session, uuid)
+            return book_to_delete
+
+        except Exception as e:
+            raise e
 
     async def update_books_by_uuid(self, data: dict, uuid: str):
         query = select(Book).where(Book.uuid == uuid)
