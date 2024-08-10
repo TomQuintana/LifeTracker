8a81081b2ff559b8edcfb7a58eec2812f20f41af
tomQuintana <tomas.quintana@gojiraf.com>
2024-08-10 02:14:16 -0300

    ✨ Add function for formatted a book data list

--- src/utils/format_data.py
+++ src/utils/format_data.py
@@ -1,6 +1,24 @@
-def format_data(data):
+def format_data_list(data) -> list:
     data_formated = []
     for element in data:
         data_formated.append(element)
 
     return data_formated
+
+
+def format_data_dict(book_data) -> dict:
+    book_formated = {}
+    for element in book_data:
+        print(element.notes)
+        book_formated = {
+            "uuid": element.uuid,
+            "title": element.title,
+            "author": element.author,
+            "type": element.type,
+            "notes": getattr(element, "notes", "No notes available"),
+            "status": element.status,
+            "physically": element.physically,
+        }
+        print(book_formated)
+
+    return book_formated
