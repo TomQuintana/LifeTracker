fcc2cfaf023c0abf0fd836628eb5fa8aea174fea
tomQuintana <tomas.quintana@gojiraf.com>
2024-08-24 12:12:09 -0300

    ♻️ Refactor user endpoint

--- src/routes/user.py
+++ src/routes/user.py
@@ -1,18 +1,11 @@
 from fastapi import APIRouter, Depends, status
-from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 from sqlmodel.ext.asyncio.session import AsyncSession
 
 from src.db.db_manager import get_session
 from src.services.user_service import UserService
 
-from ..dependency.user.dependency_manager import DependencyManager
-from ..modelRequest.user import UserRequestModel
-
-router = APIRouter(prefix="/api/user", tags=["User"])
-
-
-dependency_inyection = DependencyManager()
+router = APIRouter(prefix="/api/auth", tags=["User"])
 
 
 class UserRequest(BaseModel):
@@ -21,16 +14,14 @@ class UserRequest(BaseModel):
 
 
 @router.post("/create", status_code=status.HTTP_201_CREATED)
-async def create_user(
-    user_request: UserRequestModel,
-    user_service=Depends(dependency_inyection.use_user_service),
-):
-    await user_service.create_user(user_request)
+async def create_user():
+    raise NotImplementedError("Not implemented yet")
+    # await user_service.create_user(user_request)
 
 
 @router.post("/login")
 async def authenticate(request_user: UserRequest, session: AsyncSession = Depends(get_session)):
     user_service = UserService(session)
     token_user = await user_service.login(request_user.email, request_user.password)
-
-    return JSONResponse(content={"token": token_user}, status_code=200)
+    # return JSONResponse(content={'token': token_user}, status_code=200)
+    return {"token": token_user}
