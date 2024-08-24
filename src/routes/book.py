cf8cfa8d84f6b7d13db91dfa2db09234b7c40281
tomQuintana <tomas.quintana@gojiraf.com>
2024-08-24 11:54:38 -0300

    ♻️ Refactor endpoint books

--- src/routes/book.py
+++ src/routes/book.py
@@ -2,12 +2,12 @@ from fastapi import APIRouter, Depends, status
 from sqlmodel.ext.asyncio.session import AsyncSession
 
 from src.db.db_manager import get_session
+from src.services.auth import Auth
+from src.services.book import BookService
 
 # from src.modelRequest.book import BookRequest
 from ..interfaces.requests_schemas import CreateBookRequest, UpdateBookRequest
-from ..interfaces.response_schemas import BookResponse, BookResponseAll, BookByUuidResponse
-from src.services.auth import Auth
-from src.services.book import BookService
+from ..interfaces.response_schemas import BookResponse, BookResponseAll
 
 auth_service = Auth()
 
@@ -19,7 +19,6 @@ async def create_book(
     book: CreateBookRequest,
     session: AsyncSession = Depends(get_session),
 ):
-    print("book", book)
     book_service = BookService(session)
     book_created = await book_service.create_book(book)
     return BookResponse(**book_created.model_dump())
@@ -38,8 +37,7 @@ async def get_books(
 async def get_books_by_filter(uuid: str, session: AsyncSession = Depends(get_session)):
     book_service = BookService(session)
     book = await book_service.get_books_by_filter(uuid)
-    print("books", book)
-    return BookByUuidResponse(**book)
+    return book
 
 
 @router.delete("/{uuid}", status_code=status.HTTP_200_OK)
