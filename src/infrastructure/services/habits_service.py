2be8ce552ed8da0341628b461c66b218cbbb2ee0
tomQuintana <tomQuintana20@gmail.com>
2024-12-03 18:34:42 -0300

    🐛 Remove unnecesary routes of habits

--- src/infrastructure/services/habits_service.py
+++
@@ -1,9 +0,0 @@
-from ...domain.habits.habits_repository import HabitsRepository
-
-
-class HabitsService:
-    def __init__(self, repository: HabitsRepository):
-        self.repository = repository
-
-    async def create_habits(self, data, token):
-        return "ok"
