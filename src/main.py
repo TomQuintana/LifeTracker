fe930b0c9e1c8a090feaeb0c84f72f488251e8d5
tomQuintana <tomQuintana20@gmail.com>
2024-12-03 17:37:33 -0300

    🐛 Fix deploy

--- src/main.py
+++ src/main.py
@@ -3,18 +3,17 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-# from .infrastructure.db.db_manager import init_db
+from .infrastructure.db.db_manager import init_db
 from .infrastructure.middleware.origins import origins
-
-# from .infrastructure.routes.book import router as book_router
-# from .infrastructure.routes.expense import router as expense_router
-# from .infrastructure.routes.user import router as user_router
+from .infrastructure.routes.book import router as book_router
+from .infrastructure.routes.expense import router as expense_router
+from .infrastructure.routes.user import router as user_router
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     print("server is starting")
-    # await init_db()
+    await init_db()
     print("server docc - http://localhost:3000/docs")
     yield
     print("server is shutting down")
@@ -36,6 +35,6 @@ async def root():
     return {"message": "Welcome to Life Tracker"}
 
 
-# app.include_router(user_router)
-# app.include_router(expense_router)
-# app.include_router(book_router)
+app.include_router(user_router)
+app.include_router(expense_router)
+app.include_router(book_router)
