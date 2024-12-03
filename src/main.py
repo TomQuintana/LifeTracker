5daa4c1393504568ae5bea5f1f773273762e0815
tomQuintana <tomQuintana20@gmail.com>
2024-12-03 14:09:45 -0300

    🐛 Fix routing config for deploy
    🐛 Remove .env

--- src/main.py
+++ src/main.py
@@ -3,9 +3,8 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from .infrastructure.middleware.origins import origins
-
 from .infrastructure.db.db_manager import init_db
+from .infrastructure.middleware.origins import origins
 from .infrastructure.routes.book import router as book_router
 from .infrastructure.routes.expense import router as expense_router
 from .infrastructure.routes.user import router as user_router
@@ -29,6 +28,12 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+
+@app.get("/", status_code=200)
+async def root():
+    return {"message": "Welcome to Life Tracker"}
+
 
 app.include_router(user_router)
 app.include_router(expense_router)
