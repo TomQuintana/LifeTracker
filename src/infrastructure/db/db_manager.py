726ac3c4ca76e4701c03c662470f88c564ea1a57
tomQuintana <tomQuintana20@gmail.com>
2024-12-17 15:10:48 -0300

    🐛 Fix how to manage a session

--- src/infrastructure/db/db_manager.py
+++ src/infrastructure/db/db_manager.py
@@ -7,15 +7,19 @@ from ..config.dev_env import settings
 
 async_engine = create_async_engine(settings.POSTGRESQL_URL, future=True)
 
+async_session = sessionmaker(bind=async_engine, class_=AsyncSession, expire_on_commit=False)
+
 
 async def init_db():
     async with async_engine.begin() as session_db:
         await session_db.run_sync(SQLModel.metadata.create_all)
 
 
-async def get_session() -> AsyncSession:  # type: ignore
+async def get_session() -> AsyncSession:
     """Dependency to provide the session object"""
-    async_session = sessionmaker(bind=async_engine, class_=AsyncSession, expire_on_commit=False)
-
-    async with async_session() as session:
+    session = async_session()
+    try:
         yield session
+    except Exception as e:
+        print(e)
+        await session.close()
